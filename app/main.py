53361cabd280842ab26af551dad314163e78f086
vinit <vinit.james24@gmail.com>
2022-06-25 22:37:43 +0200

    added alembic for database migration

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .routes import posts, users, auth, votes
 from .database import engine
@@ -8,6 +9,17 @@ from .database import engine
 models.base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+      CORSMiddleware,
+      allow_origins=origins,
+      allow_credentials=True,
+      allow_methods=["*"],
+      allow_headers=["*"],
+  )
+
 
 app.include_router(posts.router)
 app.include_router(users.router)
