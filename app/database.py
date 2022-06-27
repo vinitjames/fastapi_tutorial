dc2756598a0e424aa6934d2687cbcc53d7940b28
vinit <vinit.james24@gmail.com>
2022-06-28 00:45:14 +0200

    added database password in postgres path

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import db_config
 
-SQL_DATABASE_URI = f'postgresql://{db_config.postgres_username}@{db_config.postgres_hostname}:{db_config.postgres_port}/{db_config.postgres_dbname}'
+SQL_DATABASE_URI = f'postgresql://{db_config.postgres_username}:{db_config.postgres_password}@{db_config.postgres_hostname}:{db_config.postgres_port}/{db_config.postgres_dbname}'
 
 
 engine = create_engine(SQL_DATABASE_URI)
